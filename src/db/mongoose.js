f1f49f0a0bbac6463544dca7c85aef725fe98489
s1ep0y <danmol04@gmail.com>
2019-10-20 09:37:34 +0300

    pass change

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/portfolio-local', {
+mongoose.connect('mongodb+srv://s1ep0y:danm0l@baseclaster-viaxv.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
